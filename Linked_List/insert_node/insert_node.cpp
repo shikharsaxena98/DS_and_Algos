63de562f0608b5823948294eaaec796bb99cad06
mayank-kapur <kapurm17@gmail.com>
2019-12-17 00:21:01 +0530

    Updates

--- Linked_List/insert_node/insert_node.cpp
+++ Linked_List/insert_node/insert_node.cpp
@@ -1,5 +1,5 @@
 #include<iostream>
-#include "count_nodes.h" 
+
 
 
 class Node {
@@ -7,6 +7,31 @@ public:
 	int data;
        	Node * nxt_addr;	       
 };
+
+void count_nodes(Node * c){
+	int val {0};
+	while(c != NULL){
+		c = c->nxt_addr;
+		val++;	
+	}
+
+	std::cout<<val<<"\n";
+}
+
+void insert_node(Node * c, int new_data ){
+	Node * new_node =NULL;
+        
+	new_node->data = new_data;
+	new_node->nxt_addr= NULL;
+	
+	while(c != NULL){
+		c= c->nxt_addr;
+		if(c == NULL){
+			c->nxt_addr = new_node;
+		}
+	}
+	
+}
 
 int main(){
 	Node * A = NULL;
@@ -25,7 +50,15 @@ int main(){
 
 	C->data= 3;
 	C->nxt_addr= NULL;
+	
+	Node * D = NULL;
+       	D = new Node();	
 
-	std::cout<<count_nodes(A);
+	std::cout<<"Before addition"<<"\n";
+	count_nodes(A);
+
+	insert_node(D, 5);
+	
+	count_nodes(A);
 	return 0;
 }
